4b11fda789b3c7fe54dbc4fb31ddda770d29ecd1
Sandhy <tiwarisandhyprakash@gmail.com>
2023-02-25 23:12:11 +0530

    first commit

---
+++ src/main/java/com/driver/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.driver;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+
+@Repository
+public class OrderRepository {
+//   private HashMap<String,Order>orderHashMap;
+//   order
+}
